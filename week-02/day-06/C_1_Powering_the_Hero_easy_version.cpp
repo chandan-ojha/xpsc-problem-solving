bb41ce9f2f1c85e5246f6d24856e5c8b3f5344ee
Chandan Ojha <chandanojha776@gmail.com>
2024-11-08 19:57:51 +0600

    Create C_1_Powering_the_Hero_easy_version.cpp

---
+++ week-02/day-06/C_1_Powering_the_Hero_easy_version.cpp
@@ -0,0 +1,48 @@
+// Problem link: https://codeforces.com/problemset/problem/1800/C1
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<ll> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        ll ans = 0;
+        priority_queue<ll> pq;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] == 0)
+            {
+                if (pq.empty())
+                {
+                    continue;
+                }
+                else
+                {
+                    ans += pq.top();
+                    pq.pop();
+                }
+            }
+            else
+            {
+                pq.push(a[i]);
+            }
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
