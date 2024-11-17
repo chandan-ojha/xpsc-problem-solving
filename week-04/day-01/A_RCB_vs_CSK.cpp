a5595f4a970d4cdc6774f9ca9757cd32e44b7094
Chandan Ojha <chandanojha776@gmail.com>
2024-11-17 11:04:39 +0600

    Create A_RCB_vs_CSK.cpp

---
+++ week-04/day-01/A_RCB_vs_CSK.cpp
@@ -0,0 +1,22 @@
+// Problem link: https://www.codechef.com/problems/RCBCSK
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x, y;
+    cin >> x >> y;
+
+    if (x - y >= 18)
+    {
+        cout << "RCB" << endl;
+    }
+    else
+    {
+        cout << "CSK" << endl;
+    }
+
+    return 0;
+}
