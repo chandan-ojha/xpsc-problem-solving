f5172a4dcee922cca39ec49b0c3f93120015f222
Chandan Ojha <chandanojha776@gmail.com>
2024-11-21 17:12:09 +0600

    Create F_Segments_with_Small_Spread.cpp

---
+++ week-04/day-05/F_Segments_with_Small_Spread.cpp
@@ -0,0 +1,60 @@
+// Problem link: https://codeforces.com/edu/course/2/lesson/9/2/practice/contest/307093/problem/F
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    ll k;
+    cin >> n >> k;
+
+    vector<ll> a(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    int l = 0, r = 0;
+    ll ans = 0;
+    multiset<ll> ml;
+
+    while (r < n)
+    {
+        ml.insert(a[r]);
+        ll mn = *ml.begin(), mx = *ml.rbegin();
+
+        if (mx - mn <= k)
+        {
+            ans += (r - l + 1);
+        }
+        else
+        {
+            while (l <= r)
+            {
+                mn = *ml.begin(), mx = *ml.rbegin();
+                if (mx - mn <= k)
+                {
+                    break;
+                }
+                ml.erase(ml.find(a[l]));
+                l++;
+            }
+
+            mn = *ml.begin(), mx = *ml.rbegin();
+
+            if (mx - mn <= k)
+            {
+                ans += (r - l + 1);
+            }
+        }
+        r++;
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
