6560fd83698e747890872cd149a78c10501bbe24
Chandan Ojha <chandanojha776@gmail.com>
2024-12-03 11:06:57 +0600

    Create E_Mark_the_Dust_Sweeper.cpp

---
+++ week-06/day-03/E_Mark_the_Dust_Sweeper.cpp
@@ -0,0 +1,55 @@
+// Problem Link: https://codeforces.com/problemset/problem/1705/B
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        reverse(a.begin(), a.end());
+
+        while (!a.empty() && a.back() == 0)
+        {
+            a.pop_back();
+        }
+
+        if (a.empty())
+        {
+            cout << 0 << endl;
+            continue;
+        }
+
+        ll ans = 0;
+        reverse(a.begin(), a.end());
+        n = a.size();
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (a[i] == 0)
+            {
+                ans++;
+            }
+            else
+            {
+                ans += a[i];
+            }
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
