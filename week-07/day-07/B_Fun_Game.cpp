281cd31c62bd1c0452a932f401de0871f31b5e8c
Chandan Ojha <chandanojha776@gmail.com>
2024-12-14 17:18:49 +0600

    Create B_Fun_Game.cpp

---
+++ week-07/day-07/B_Fun_Game.cpp
@@ -0,0 +1,65 @@
+// Problem Link: https://codeforces.com/problemset/problem/1994/B
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int n;
+        cin >> n;
+        string s, t;
+        cin >> s >> t;
+
+        if (s == t)
+        {
+            cout << "YES" << '\n';
+            continue;
+        }
+
+        int pos = -1;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '1')
+            {
+                pos = i;
+                break;
+            }
+        }
+
+        if (pos != -1)
+        {
+            bool ok = true;
+            for (int i = 0; i < n; i++)
+            {
+                if (s[i] != t[i])
+                {
+                    if (pos > i)
+                    {
+                        ok = false;
+                        break;
+                    }
+                }
+            }
+            if (ok)
+            {
+                cout << "YES" << '\n';
+            }
+            else
+            {
+                cout << "NO" << '\n';
+            }
+        }
+        else
+        {
+            cout << "NO" << '\n';
+        }
+    }
+
+    return 0;
+}
