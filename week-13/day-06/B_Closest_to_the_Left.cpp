c37ff2dd0f0911b3300992a9295e4db5be72fd3d
Chandan Ojha <chandanojha776@gmail.com>
2025-01-31 18:11:26 +0600

    Create B_Closest_to_the_Left.cpp

---
+++ week-13/day-06/B_Closest_to_the_Left.cpp
@@ -0,0 +1,43 @@
+// Problem Link: https://codeforces.com/edu/course/2/lesson/6/1/practice/contest/283911/problem/B
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, q;
+    cin >> n >> q;
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < q; i++)
+    {
+        int key, l = 0, r = n - 1, ans = -1, mid;
+        cin >> key;
+
+        while (l <= r)
+        {
+            mid = (l + r) / 2;
+
+            if (key >= a[mid])
+            {
+                ans = mid;
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid - 1;
+            }
+        }
+
+        cout << ans + 1 << '\n';
+    }
+
+    return 0;
+}
